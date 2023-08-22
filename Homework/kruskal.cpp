63e6563b6ee51210f9b62abff4b5bd6eca4cc23b
Fer Cortés <a01026613@tec.mx>
2023-08-22 16:46:46 -0500

    sum, hello world, kruskal hw

---
+++ Homework/kruskal.cpp
@@ -0,0 +1,10 @@
+// Created by Fer Cortés, Mario Frías and Juan Pablo Cruz 
+// Date: 22/09/2023
+
+// Kruskal algorithm implementation
+// This algorithm is used to find the minimum spanning tree of a graph, it recieves a .txt file with the graph information.
+// The file must have the following format:
+// 1 2 3 (node1 node2 weight)
+// And the output will be:
+// The used connections and their final weight
+
